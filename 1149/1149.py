661056a015b1899bf04ee66add77d5b42517911b
DonghwaJeong <kbc1685@naver.com>
2020-03-16 14:49:21 +0900

    1149

---
+++ 1149/1149.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+board = []
+dp = []
+for _ in range(N):
+    price = sys.stdin.readline().split()
+    board.append(list(map(int, price)))
+
+dp = board
+for i in range(1,N):
+    dp[i] = [min(dp[i-1][x-1], dp[i-1][x-2]) + board[i][x] for x in range(3)]
+
+print(min(dp[-1]))
