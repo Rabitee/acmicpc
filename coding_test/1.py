17b99937a3872afe895e9e23d6dfbf65e816a08e
DonghwaJeong <kbc1685@naver.com>
2020-03-26 15:17:03 +0900

    Volia!

--- coding_test/1.py
+++ coding_test/1.py
@@ -6,14 +6,14 @@ for _ in range(N):
     part_times.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
 def solution(part_times):
-    part_times = sorted(part_times, key=lambda x:(x[1], x[0], x[2]))
-    dp = [t[2] for t in part_times]
+    part_times = sorted(part_times, key=lambda x:x[1])
+    p = [t[2] for t in part_times]
     q = [t[:2] for t in part_times]
 
     for i in range(1, len(q)):
         for j in range(0, i):
             if q[j][1] <= part_times[i][0]:
-                dp[i] = max(dp[i], dp[j] + part_times[i][2])
+                p[i] = max(p[i], p[j] + part_times[i][2])
 
-    return max(dp)
+    return max(p)
 print(solution(part_times))
--- coding_test/2.py
+++ coding_test/2.py
@@ -6,5 +6,32 @@ for _ in range(N - 1):
     skills.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
 def solution(total_sp, skills):
+    tree = {i + 1:None for i in range(len(skills) + 1)}
+    for skill in skills:
+        if tree[skill[0]] == None:
+            tree[skill[0]] = [skill[1]]
+        else:
+            tree[skill[0]].append(skill[1])
+    sp = [0] * (len(skills) + 1)
+
+    def root(skills):
+        l = [-1] * (len(skills) + 1)
+        for skill in skills:
+            l[skill[1] - 1] = skill[0] - 1
+        return l.index(-1) + 1
+
+    def f(tree, sp, node):
+        if tree[node] == None:
+            sp[node - 1] = 1
+        else:
+            for e in tree[node]:
+                f(tree, sp, e)
+            sp[node - 1] = sum(sp[e - 1] for e in tree[node])
+    
+    r = root(skills)
+    f(tree, sp, r)
+    v = int(total_sp / sum(sp))
+    sp = list(map(lambda x:x*v, sp))
+    return sp
     
 print(solution(total_sp, skills))
